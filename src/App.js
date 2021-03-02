0ed04b9ec237f0d197bf454fea0150e90ddcbc42
Ashley Pauldo <ashleypauldo@MacBook-Pro-4.lan>
2021-03-02 17:11:37 -0500

    add tictactoe

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Board from './Components/Board/Board'
+
+
+function App() {
+  return (
+    // <> is a react fragment. 
+    <>  
+      <Board />
+    </>
+  );
+}
+export default App;
